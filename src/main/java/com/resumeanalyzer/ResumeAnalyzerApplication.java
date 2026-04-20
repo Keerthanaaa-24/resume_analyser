2dee19c2c2b3879ebac8117c225c3f7132400099
Keerthu <keerthanamr1307@gmail.com>
2026-04-20 22:33:43 +0530

    ResumeAnalyzerApplication.java

--- src/main/java/com/resumeanalyzer/ResumeAnalyzerApplication.java
+++ src/main/java/com/resumeanalyzer/ResumeAnalyzerApplication.java
@@ -1,8 +1,6 @@
 package com.resumeanalyzer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ResumeAnalyzerApplication {
     public static void main(String[] args) {
