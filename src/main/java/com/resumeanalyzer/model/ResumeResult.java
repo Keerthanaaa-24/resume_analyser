269794e1358510ef65ab62ebc5a4069819c3c807
Keerthu <keerthanamr1307@gmail.com>
2026-04-20 22:31:57 +0530

    ResumeResult.java

--- src/main/java/com/resumeanalyzer/model/ResumeResult.java
+++ src/main/java/com/resumeanalyzer/model/ResumeResult.java
@@ -1,15 +1,11 @@
 package com.resumeanalyzer.model;
-
 import java.util.*;
-
 public class ResumeResult {
-
     private int score;
     private String feedback;
     private Map<String, Integer> categories;
     private Map<String, List<String>> skills;
     private String roadmap;
-
     public ResumeResult(int score, String feedback,
                         Map<String, Integer> categories,
                         Map<String, List<String>> skills,
@@ -20,7 +16,6 @@ public class ResumeResult {
         this.skills = skills;
         this.roadmap = roadmap;
     }
-
     public int getScore() { return score; }
     public String getFeedback() { return feedback; }
     public Map<String, List<String>> getSkills() { return skills; }
